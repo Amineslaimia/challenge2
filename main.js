514e9e5e0f814439ddbf30530714ac817a5dd3fa
amineslaimia <medamineslaimia@gmail.com>
2019-10-21 22:12:51 +0200

    challenge2

--- main.js
+++ main.js
@@ -44,15 +44,25 @@ Create two computers object from your factory function and save them in one arra
 
 Note: please write one or two lines here describing your solution.
 */
+//this function takes the parameters and returns them as the object computer's keys
 
 function makeComputer(type, color, weight) {
   // TODO: Your code here
+  var computer={
+  	tyrpe:type,
+  	color:color,
+  	weight:weight
+  };
+
+  	return computer;
 }
 
 // Write a function displayComputer that can be used to display one computer.
+//this function takes the object computer and returns it as a string with the values of its keys 
 
 function displayComputer(computer) {
   // TODO: Your code here
+  return 'this computer is '+computer.type+' of color '+computer.color+' and weighs '+computer.weight;
 }
 
 //=============================================================================
@@ -71,9 +81,13 @@ Note: please write one or two lines here describing your solution.
 var strArr = ['hello', 'world', 'whirled', 'peas'];
 uppercaseAll(strArr); ==> [ 'HELLO', 'WORLD', 'WHIRLED', 'PEAS' ]
 */
+//takes every  element of the array and executes the function to uppercase on it using map to return an array of the result 
 
 function uppercaseAll(arrayOfStrings) {
   // TODO: your code here
+  return map(arrayOfStrings,function(element){
+  	return element.toUpperCase();
+  })
 }
 
 //=============================================================================
@@ -110,9 +124,12 @@ var data = [
     population: 263991379
   }
 ];
-
+// this function takes an array of objects and filters it useing filter with the predecit of population greater than 500 millions
 function highestPopulation(arrayOfObjects) {
   // TODO: your code here
+  return filter (arrayOfObjects,function(obj,key){
+  			return obj.population> 500000000
+  })
 }
 
 //=============================================================================
@@ -130,9 +147,12 @@ halveAll(numsArray); ==> [ 1, 3, 10, 4, 7 ]
 
 Note: please write one or two lines here describing your solution.
 */
-
+//here i used map because i need an array as a return and to execute a function on every elements of the array in the parameters
 function halveAll(numbers) {
   // your code is here
+  return map (numbers,function(number,index){
+  	return number/2;
+  });
 }
 
 //=============================================================================
@@ -147,8 +167,12 @@ values({first : 5, second: 'something' , third : 129}) ==> [5, 'something', 129]
 
 Note: please write one or two lines here describing your solution.
 */
+// here map is used to return an array and the values of the object entered in the parameters as its values 
 function values(obj) {
   // TODO: your code here
+  return map( obj,function(obj,key){
+  	return obj;
+  })
 }
 
 //Good Luck :))
